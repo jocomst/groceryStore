5c1e1b20d6e2d59598f14ca0321a61fbc09701cf
John <johncomstock9716@gmail.com>
2022-09-12 13:38:55 -0400

    updated functions

--- groceryStore.js
+++ groceryStore.js
@@ -1,12 +1,12 @@
 /*
 Assignment requirements:
-1. Create an arry called "shoppingList" with the following items:
+1. Create an array called "shoppingList" with the following items:
 'pop tarts', 'ramen noodles', 'chips', 'salsa', and 'coffee'.
-2. Add 'fruit loops' to the shopping list
-3. Update 'coffee' to 'fair trade coffee'
-4. Replace 'chips' and 'salsa' with 'rice' and 'beans'
+2. Write a function called 'addFruitLoops' that adds 'fruit loops' to an array
+3. Write a function called updateElement that takes an array, an element from that array, and a new element
+ and updates it : ex. "coffee" -> "fair trade coffee"
 5. Create an empty array called 'shoppingCart'
-6. Remove the last item from shoppingList and add it to shoppingCart
+6. Write a function called removeLast that takes the last element of one array and adds it to another array
 7. Remove the first item from shoppingList and add it to shoppingCart
 8. Write a 'while' loop that takes items from your shopping list and moves them 
 to your cart until there are no items left in the list.
@@ -16,11 +16,26 @@ as a comma separated string
 */
 
 const shoppingList = ["pop tarts", "ramen noodles", "chips", "salsa", "coffee"];
-shoppingList.push("fruit loops");
+const addFruitLoops = (arr) => {
+	arr.push("fruit loops");
+};
+
+const updateElement = (arr, word, newWord) => {
+	const index = arr.find(word);
+	arr[index] = newWord;
+};
 
 const replaceWord = (word, newWord, arr) => {
 	const indexNum = arr.findIndex((x) => x === word);
 	arr[indexNum] = newWord;
+};
+
+const removeLast = (arrOne, arrTwo) => {
+	arrTwo.push(arrOne.pop());
+};
+
+const removeFirst = (arrOne, arrTwo) => {
+	arrTwo.push(arrOne.shift());
 };
 
 replaceWord("coffee", "fair trade coffee", shoppingList);
